ecf2621f6f28952c2f0fdb884bb8cbc0a3b99941
NagamaniKambam <57787197+NagamaniKambam@users.noreply.github.com>
2019-11-15 12:23:49 +0530

    Add files via upload

---
+++ Fibonacci_Program_4.java
@@ -0,0 +1,25 @@
+package com.onmiwyse.AssignmentONE1;
+
+//Java program for Fibonacci Series using Space
+//Time Complexity is O(n)
+//Space Complexity is O(1)
+
+public class Fibonacci_Program_4 
+{
+	static int fib(int number)
+	{
+		if (number <= 1)
+		{
+			return number;
+		} 
+		else
+		{
+			return fib(number - 1) + fib(number - 2);
+		}
+	}
+
+	public static void main(String args[]) 
+	{
+		System.out.println("Fibonacci of " + 8 + " is : " + fib(8));
+	}
+}
