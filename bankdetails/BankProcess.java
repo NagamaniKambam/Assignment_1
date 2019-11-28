2fcebe4f86b47ecde23cbdfdbe93eba08fb1532a
NagamaniKambam <57787197+NagamaniKambam@users.noreply.github.com>
2019-11-28 00:56:12 -1200

    Delete BankProcess.java

--- bankdetails/BankProcess.java
+++
@@ -1,8 +0,0 @@
-package bankdetails;
-
-public interface BankProcess {
-    public void showBalance();
-    public void withdraw();
-    public void deposite();
-    public void remainingBalance();
-}
